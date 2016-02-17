b5b9dbd1b401e42f9493bf27677a7a663c9241c5
Neel Mehta <neelmehta@college.harvard.edu>
2016-02-16 20:19:37 -0500

    Scaffold with generator-react-express

---
+++ client/scripts/index.js
@@ -0,0 +1,53 @@
+var React = require('react'),
+	Router = require('react-router');
+
+var Header = React.createClass({
+	render: function() {
+		return (
+			<div className="page-header">
+				<h1>HODP</h1>
+			</div>
+		);
+	}
+});
+
+var PageNav = React.createClass({
+	render: function() {
+		return (
+			<div className="nav">
+				<Router.Link to="home">Home</Router.Link>
+				&nbsp; | &nbsp;
+				<Router.Link to="about">About</Router.Link>
+			</div>
+		);
+	}
+});
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div className="container">
+				<Header />
+				<PageNav />
+				<Router.RouteHandler/>
+			</div>
+		);
+	}
+});
+
+var routes = {
+	Home: require('../routes/Home'),
+	About: require('../routes/About')
+};
+
+var routes = (
+	<Router.Route name="app" path="/" handler={App}>
+		<Router.Route name="home" path="/" handler={routes.Home}/>
+		<Router.Route name="about" path="/about" handler={routes.About}/>
+		<Router.DefaultRoute handler={routes.Home}/>
+	</Router.Route>
+);
+
+Router.run(routes, Router.HistoryLocation, function (Handler) {
+	React.render(<Handler/>, document.body);
+});
