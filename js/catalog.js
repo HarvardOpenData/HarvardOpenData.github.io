8ff04bf3e9f16eb9eef0e822afe39efa2f5ed8aa
Neel Mehta <neelmehta@college.harvard.edu>
2017-03-11 12:40:27 -0500

    add jquery and new scripts

---
+++ js/catalog.js
@@ -0,0 +1,6 @@
+// script for the catalog
+// $(document).ready(function(){
+    d3.csv("/assets/harvard-open-data-catalog.csv", function callback(data){
+        console.log(data);
+    });
+// });
