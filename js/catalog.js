06c9b87170078e70a3aafdfcf71b164fe3dcd6fc
Neel Mehta <neelmehta@college.harvard.edu>
2017-03-11 14:53:56 -0500

    start listing stuff

--- js/catalog.js
+++ js/catalog.js
@@ -1,6 +1,20 @@
 // script for the catalog
 // $(document).ready(function(){
-    d3.csv("/assets/harvard-open-data-catalog.csv", function callback(data){
-        console.log(data);
-    });
-// });
+
+d3.csv("/assets/harvard-open-data-catalog.csv", function callback(data){
+    console.log(data);
+
+    // load entries into catalog
+    var catalog = d3.select("#catalog-results")
+        .selectAll("div.result")
+        .data(data);
+
+    catalog.exit().remove();
+
+    // TODO use some kind of templating engine so generating this isn't as ugly
+    catalog.enter().append("div")
+        .attr("class", "result")
+        .text(function(d){
+            return d.title;
+        });
+});
