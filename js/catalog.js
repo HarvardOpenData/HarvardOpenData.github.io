f1776ba226fe146591dc6bb5f0f7511cd036461d
Neel Mehta <neelmehta@college.harvard.edu>
2017-03-11 15:00:57 -0500

    add buttons and text

--- js/catalog.js
+++ js/catalog.js
@@ -4,6 +4,11 @@
 d3.csv("/assets/harvard-open-data-catalog.csv", function callback(data){
     console.log(data);
 
+    // sort data by title
+    data.sort(function(a, b) {
+        return a.title > b.title;
+    });
+
     // load entries into catalog
     var catalog = d3.select("#catalog-results")
         .selectAll("div.result")
@@ -12,9 +17,37 @@ d3.csv("/assets/harvard-open-data-catalog.csv", function callback(data){
     catalog.exit().remove();
 
     // TODO use some kind of templating engine so generating this isn't as ugly
-    catalog.enter().append("div")
-        .attr("class", "result")
+    var catalogEnter = catalog.enter();
+    var catalogDiv = catalogEnter.append("div")
+        .attr("class", "result panel panel-default");
+
+    // add heading
+    catalogDiv.append("div")
+        .attr("class", "panel-heading")
+        .append("h3")
+            .attr("class", "panel-title")
+            .text(function(d){
+                return d.title;
+            });
+
+    // add body
+    var catalogBody = catalogDiv.append("div")
+        .attr("class", "panel-body");
+
+    // text description
+    catalogBody.append("p")
         .text(function(d){
-            return d.title;
+            return d.description;
         });
+
+    // download button
+    catalogBody.append("a")
+        .attr("href", function(d) {
+            return d.url;
+        })
+        .attr("class", "btn btn-primary")
+        .text(function(d){
+            return "Download"
+        });
+
 });
