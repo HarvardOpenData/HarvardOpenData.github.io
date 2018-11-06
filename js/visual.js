6edfffe77a3fdf180933e3487082a41ee5cf6b94
Kevin Bi <keveloper@gmail.com>
2018-11-06 17:23:28 -0500

    Fixed some compilation bugs, still problematic

--- js/visual.js
+++ js/visual.js
@@ -1,6 +1,3 @@
-<src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
-<src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.1.2/papaparse.js">
-
 var xKey;
 var yKeys = [];
 var xLabel;
@@ -50,24 +47,13 @@ function loadData (file) {
 // Populate a table in html with data
 // William and Festus 
 function populateTable(data){
-    function arrayToTable(tableData) {
-        var table = $('<table></table>');
-        $(tableData).each(function (i, rowData) {
-            var row = $('<tr></tr>');
-            $(rowData).each(function (j, cellData) {
-                row.append($('<td>'+cellData+'</td>'));
-            });
-            table.append(row);
+    var table = $('#CSVTable');
+    $(data).each(function (i, rowData) {
+        var row = $('<tr></tr>');
+        $(rowData).each(function (j, cellData) {
+            row.append($('<td>'+cellData+'</td>'));
         });
-        return table;
-    }
-
-    $.ajax({
-        type: "GET",
-        url: "http://localhost/test/data.csv",
-        success: function (data) {
-            $('body').append(arrayToTable(Papa.parse(data).data));
-        }
+        table.append(row);
     });
 }
 
@@ -115,44 +101,7 @@ function populateChart(data){
         }
     }
 
-    var chart = new Chart(context, config) {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    });
-    ;
-
+    var chart = new Chart(context, config);
 }
 
 window.onload = function(){
