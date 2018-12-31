80e589cb1f7936370e69c17c2c1a005680fb601d
Kevin Bi <keveloper@gmail.com>
2018-12-30 21:03:53 -0600

    Added comments

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 window.onload = function(){
+    // Load the data from the Medium RSS feed
+    // Uses the Google Apps Script to avoid cross site origin requests
     $.ajax({
         url: "https://script.google.com/macros/s/AKfycbwafKoSHjdBxd9sDBB-ohKjaDsOlaeXkRD4BMLPG28ZNgK7wFw/exec",
         type : "GET",
         success : function (data, status, xhr) {
           var articles = JSON.parse(data);
+          // Gets the data and puts it in the DOM
           for(var i = 0; i < 3; i++){
               var article = articles[i];
               var link = document.getElementById("articlelink" + String(i));
