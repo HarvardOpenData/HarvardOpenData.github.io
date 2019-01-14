2dde66e1d0c96772cc310ad6b4400e50a7ac84d4
Maddy Nakada <maddynakada@gmail.com>
2019-01-14 15:56:21 -0800

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
