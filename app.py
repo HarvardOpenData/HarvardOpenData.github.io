f06ed8f795a2a8aad554edfbfbfddd5c8ea429ff
Maddy Nakada <maddynakada@gmail.com>
2019-01-15 12:44:17 -0800

    home and about pages

--- app.py
+++ app.py
@@ -3,24 +3,27 @@ from flask import render_template
 import yaml
 
 app = Flask(__name__)
-def siteConstants():
-    site = getYml('./data/site.yml')
-    site["pages"] = getYml('./data/pages.yml')
-    return site
 
 def getYml(filepath):
     with open(filepath) as infile:
         fileMap = yaml.safe_load(infile)
         return fileMap
 
+# get all site-wide yaml into dict
+def siteConstants():
+    site = getYml('./data/site.yml')
+    site["pages"] = getYml('./data/pages.yml')
+    return site
+
+site = siteConstants()
+pageData = getYml('./data/pageData.yml')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    categories = getYml('./data/categories.yml')
+    return render_template('index.html', site = site, page=pageData["index"][0], categories=categories)
 
 @app.route('/about')
 def about():
-    pageData = getYml('./data/pageData.yml')
-    print(pageData)
-    peopleMap = getYml('./data/people.yml')
-    site = siteConstants()
-    return render_template('about.html', site=site, peopleMap = peopleMap, page=pageData["about"])
+    people = getYml('./data/people.yml')
+    return render_template('about.html', site=site, people = people, page=pageData["about"][0])
