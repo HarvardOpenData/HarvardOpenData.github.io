a4dd4f0966a550062c23bb95c584bb76df91c733
Alex Rankine <alexrankine@college.harvard.edu>
2019-05-09 13:52:51 -0400

    added featured articles to index

--- main.py
+++ main.py
@@ -4,65 +4,76 @@ import yaml
 
 app = Flask(__name__)
 
+
 def getYml(filepath):
     with open(filepath) as infile:
         fileMap = yaml.safe_load(infile)
         return fileMap
 
 # get all site-wide yaml into dict
+
+
 def siteConstants():
     site = getYml('./data/site.yml')
     site["pages"] = getYml('./data/pages.yml')
     return site
 
+
 site = siteConstants()
 pageData = getYml('./data/pageData.yml')
+
 
 @app.route('/')
 def index():
     categories = getYml('./data/categories.yml')
-    return render_template('index.html', site = site, page=pageData["index"][0], categories=categories)
+    featured = enumerate(getYml('./data/featured.yml'))
+    return render_template('index.html', site=site, page=pageData["index"][0], categories=categories, featured=featured)
+
 
 @app.route('/about/')
 def about():
     people = getYml('./data/people.yml')
-    return render_template('about.html', site=site, people = people, page=pageData["about"][0])
+    return render_template('about.html', site=site, people=people, page=pageData["about"][0])
+
 
 @app.route('/calendar/')
 def calendar():
     return render_template('calendar.html', site=site, page=pageData["calendar"][0])
 
+
 @app.route('/catalog/')
 def catalog():
-    return render_template('catalog.html', site=site, categories = getYml("./data/categories.yml"),
-                                 filetypes = getYml("./data/filetypes.yml"), page = pageData["catalog"][0])
+    return render_template('catalog.html', site=site, categories=getYml("./data/categories.yml"),
+                           filetypes=getYml("./data/filetypes.yml"), page=pageData["catalog"][0])
+
 
 @app.route('/get_involved/')
 def get_involved():
-    return render_template('get_involved.html', site=site, page = pageData["get_involved"][0])
+    return render_template('get_involved.html', site=site, page=pageData["get_involved"][0])
+
 
 @app.route('/submit/')
 def submit():
-    return  render_template('submit.html', site=site, page=pageData["submit"][0], categories = getYml('./data/categories.yml'))
+    return render_template('submit.html', site=site, page=pageData["submit"][0], categories=getYml('./data/categories.yml'))
+
 
 @app.route('/visual/')
-def visual(): 
+def visual():
     print(pageData["visual"][0])
-    return render_template('visual.html', site = site, page = pageData["visual"][0])
-
-
-
+    return render_template('visual.html', site=site, page=pageData["visual"][0])
 
 
 ### WEBAPPS ###
 @app.route('/visual/crime')
 def crime():
-    return render_template('webapps/crimemap.html', site = site, page = pageData["crimemap"][0])
+    return render_template('webapps/crimemap.html', site=site, page=pageData["crimemap"][0])
+
 
 @app.route('/visual/crimson')
 def crimson():
-    return render_template('webapps/crimson.html', site = site, page = pageData["crimsonwords"][0])
+    return render_template('webapps/crimson.html', site=site, page=pageData["crimsonwords"][0])
+
 
 @app.route('/visual/studyabroad')
 def studyabroad():
-    return render_template('webapps/studyabroad.html', site = site, page = pageData["studyabroad"][0])
+    return render_template('webapps/studyabroad.html', site=site, page=pageData["studyabroad"][0])
