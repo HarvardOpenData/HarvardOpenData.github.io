c846ccc77f2d967830228adab6afe249a067f71f
Kevin Bi <keveloper@gmail.com>
2019-07-09 22:33:55 -0400

    Added updates to record having demographics

--- server/demographics.py
+++ server/demographics.py
@@ -2,10 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import server.auth as auth
+import datetime
 
 def update_demographics(email, form, demo_questions, db):
-    print("UDPATING DEMOGRAPHICS...")
     userResponsesRef = db.collection("responses").document(auth.email_hash(email))
+    userEmailsRef = db.collection("emails").document(email)
     new_demographics = {} 
     for field in form:
         if field in demo_questions["valid_form_names"]:
@@ -13,9 +14,12 @@ def update_demographics(email, form, demo_questions, db):
                 new_demographics[field] = form.getlist(field)
             elif field != "house" or form.get("year", -1) != demo_questions["years"][-1]:
                 new_demographics[field] = form[field]
-    print(new_demographics)
     userResponsesRef.update({
         "demographics" : new_demographics
+    })
+    userEmailsRef.update({
+        "has_demographics" : True,
+        "last_demographic_update" : datetime.datetime.now()
     })
 
 
