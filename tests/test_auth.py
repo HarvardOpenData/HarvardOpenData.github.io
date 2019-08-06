a4b40ea902e0582f84ebc2f04e51dd1dbf7c6225
Kevin Bi <keveloper@gmail.com>
2019-08-05 21:57:11 -0400

    add basic is_authenticated tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,36 +6,43 @@ import unittest
 
 from server import auth
 
-class TestAuth(unittest.TestCase):
-    def test_is_authenticated():
-        pass
-    import os, sys
 from mockfirestore import *
-from os.path import dirname, join, abspath
-sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 
-import unittest
-from server import auth
-
-class TestAuth(unittest.TestCase):
-    def setUp(self):
-        self.db = MockFirestore()
-        self.reset_database()
-
-    def test_is_authenticated(self):
-        pass
-
-    def reset_database(self):
-        db = self.db
-        db.reset_database()
-        emails_ref = db.collection("email")
+def reset_database(db : MockFirestore):
+        db.reset()
+        emails_ref = db.collection("emails")
         emails_ref.document("test1").set({
             "id" : "12345",
             "has_demographics" : True
         })
         
         emails_ref.document("test2").set({
-            "id": "56789",
             "has_demographics" : False
         })
 
+class TestAuth(unittest.TestCase):
+    def setUp(self):
+        self.db = MockFirestore()
+        reset_database(self.db)
+
+    def test_is_authenticated_false(self):
+        db = self.db
+        reset_database(db)
+        emails_ref = db.collection("emails")
+        self.assertFalse(auth.is_authenticated(None, None, emails_ref))
+        self.assertFalse(auth.is_authenticated("test2", "12345", emails_ref))
+        self.assertFalse(auth.is_authenticated("test3", "12345", emails_ref))
+
+    def test_is_authenticated_true(self):
+        db = self.db
+        reset_database(db)
+        emails_ref = db.collection("emails")
+        self.assertTrue(auth.is_authenticated("test1", "12345", emails_ref))
+
+    def test_is_authenticated_error(self):
+        self.assertRaises(Exception, lambda : auth.is_authenticated("test1", "23456"))
+
+
+    def test_create_respondent_new(self):
+        db = self.db
+        reset_database(db)
