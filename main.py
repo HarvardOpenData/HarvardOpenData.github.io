a342a6f3a436581c873fa5a1940bddb0ab3da158
Kevin Bi <keveloper@gmail.com>
2019-09-18 14:39:58 -0400

    Changed to incorporate tiers

--- main.py
+++ main.py
@@ -42,8 +42,17 @@ peopleYml = getYml("./data/people.yml")
 add_members_to_firestore(auth.get_website_firestore_client(), peopleYml)
 members_cache.populate(auth.get_website_firestore_client(), peopleYml)
 
-sponsorsYml = sorted(getYml("./data/sponsors.yml"), key = lambda x: x["weight"], reverse = True)
-sponsor_weights = [sponsor["weight"] for sponsor in sponsorsYml]
+tier_entries = {
+    "Platinum" : 20,
+    "Gold" : 10,
+    "Silver" : 4,
+    "Bronze" : 2,
+    "Startup" : 1
+}
+
+tiersYml = getYml("./data/sponsors.yml")["tiers"]
+sponsorsYml = getYml("./data/sponsors.yml")["sponsors"]
+sponsor_weights = [tier_entries[sponsor["tier"]] for sponsor in sponsorsYml]
 
 @app.route('/')
 def index():
@@ -51,12 +60,15 @@ def index():
     featured = enumerate(getYml('./data/featured.yml'))
     date_string = datetime.datetime.now().strftime('%H %d %m %Y')
     random.seed(date_string, 2)
-    featured_sponsors = random.choices(sponsorsYml, weights = sponsor_weights, k = 1)
-    filtered_sponsors = [sponsor for sponsor in sponsorsYml if sponsor not in featured_sponsors]
+    featured_sponsors : list = random.choices(sponsorsYml, weights = sponsor_weights, k = 1)
+    selected_index = sponsorsYml.index(featured_sponsors[0])
+    filtered_sponsors : list= sponsorsYml.copy()
+    filtered_sponsors.pop(selected_index)
     if filtered_sponsors:
-        filtered_sponsor_weights = [sponsor["weight"] for sponsor in filtered_sponsors]
+        filtered_sponsor_weights = sponsor_weights.copy()
+        filtered_sponsor_weights.pop(selected_index)
         featured_sponsors.extend(random.choices(filtered_sponsors, filtered_sponsor_weights, k = 1))
-    list.sort(featured_sponsors, key = lambda x : x["weight"], reverse = True)
+    featured_sponsors.sort(key = lambda x: tiersYml.index(x["tier"]))
     random.seed(time.time())
     return render_template('index.html', site=site, page=pageData["index"][0], categories=categories, featured=featured,
                             featured_sponsors = featured_sponsors)
@@ -69,7 +81,7 @@ def about():
 
 @app.route('/sponsors/')
 def sponsors():
-    return render_template('sponsors.html', site=site, sponsors = sponsorsYml, page=pageData["sponsors"][0])
+    return render_template('sponsors.html', site=site, sponsors = sponsorsYml, tiers = tiersYml, page=pageData["sponsors"][0])
 
 
 @app.route('/calendar/')
