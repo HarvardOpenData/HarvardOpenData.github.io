e8a637978f4e50adb63a3c49c4de1bc08c59b4dc
MrAw3someness <K3v@Cr1ms0n>
2020-05-19 13:48:01 -0500

    Corrected spelling

--- mock_run.py
+++ mock_run.py
@@ -3,7 +3,7 @@ import subprocess
 if __name__ == "__main__":
     if os.system("python3 --version") == 0:
         python_command = "python3"
-    elif os.system("python --verison") == 0:
+    elif os.system("python --version") == 0:
         python_command = "python"
     else: 
         raise Exception("No valid python version found")
