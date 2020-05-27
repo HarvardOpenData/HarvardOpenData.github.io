ee3eff0cd71033abbf185762f7e2837023e5dfe3
Melissa Kwan <>
2020-05-27 16:51:59 -0700

    Remove CloudFare folders

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -4,4 +4,15 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
+  headComponents.sort((x, y) => {
+    if (x.key === 'TypographyStyle') {
+      return -1
+    } else if (y.key === 'TypographyStyle') {
+      return 1
+    }
+    return 0
+  })
+  replaceHeadComponents(headComponents)
+}
