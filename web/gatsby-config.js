32ca089bc318085709b346ae78c711efd9115875
Melissa Kwan <>
2020-05-31 15:40:26 -0700

    Start adding Theme UI

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-theme-ui',
     {
       resolve: 'gatsby-source-sanity',
       options: {
