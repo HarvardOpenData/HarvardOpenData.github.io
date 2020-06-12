c627356fdf8bb3f50621518d40a108d82c90dbf4
Melissa Kwan <mwjkwan@gmail.com>
2020-06-11 21:40:35 -0700

    Primitive page menu finished

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -83,3 +83,38 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   await createBlogPostPages(graphql, actions, reporter)
   await createProjectPages(graphql, actions, reporter)
 }
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createFieldExtension, createTypes } = actions
+  createFieldExtension({
+    name: `defaultArray`,
+    extend() {
+      return {
+        resolve(source, args, context, info) {
+          if (source[info.fieldName] == null) {
+            return []
+          }
+          return source[info.fieldName]
+        },
+      }
+    },
+  })
+  const typeDefs = `
+    type Site implements Node {
+      siteMetadata: SiteMetadata
+    }
+    type SiteMetadata {
+      menuLinks: [MenuLinks]!
+    }
+    type MenuLinks {
+      name: String!
+      link: String!
+      subMenu: [SubMenu] @defaultArray
+    }
+    type SubMenu {
+      name: String
+      link: String
+    }
+  `
+  createTypes(typeDefs)
+}
--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,40 +1,164 @@
 /** @jsx jsx */
-import { jsx, Styled } from 'theme-ui'
-import { Link } from 'gatsby'
-import Icon from './icons'
-import { cn } from '../lib/helpers'
+// import { Link } from 'gatsby'
+// import Icon from './icons'
+// import { cn } from '../lib/helpers'
 
-import styles from './header.module.css'
+// import styles from './header.module.css'
 
-const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
-  <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <h1 className={styles.branding}>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
+// const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
+//   <div className={styles.root}>
+//     <div className={styles.wrapper}>
+//       <h1 className={styles.branding}>
+//         <Link to="/">{siteTitle}</Link>
+//       </h1>
 
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol="hamburger" />
-      </button>
+//       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
+//         <Icon symbol="hamburger" />
+//       </button>
 
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to="/about/">About</Link>
+//       <nav className={cn(styles.nav, showNav && styles.showNav)}>
+//         <ul>
+//           <li>
+//             <Link to="/about/">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/projects/">Projects</Link>
+//           </li>
+//           <li>
+//             <Link to="/blog/">Blog</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact/">Contact</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   </div>
+// )
+
+// export default Header
+
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { jsx, Grid, Styled } from 'theme-ui'
+export default function Header() {
+  const data = useStaticQuery(graphql`
+    query HeaderQuery {
+      site {
+        siteMetadata {
+          title
+          menuLinks {
+            link
+            name
+            subMenu {
+              link
+              name
+            }
+          }
+        }
+      }
+    }
+  `)
+  const menuLinks = data.site.siteMetadata.menuLinks
+
+  return (
+    <div
+      sx={{position: 'relative', zIndex: '99 !important'}}
+    >
+      <Styled.h1>
+        <Link to="/">{data.site.siteMetadata.title}</Link>
+      </Styled.h1>
+      <ul
+        sx={{
+          listStyle: "none",
+          background: "darkorange",
+          margin: 0,
+          padding: 0,
+        }}
+      >
+        {menuLinks.map((link) => (
+          <li
+            sx={{
+              color: "white",
+              backgroundColor: "darkorange",
+              display: "block",
+              float: "left",
+              padding: "1rem",
+              position: "relative",
+              transitionDuration: "0.5s",
+              ":hover": {
+                backgroundColor: "red",
+                cursor: "pointer",
+              },
+              ":hover > ul, :focus-within > ul ": {
+                visibility: "visible",
+                opacity: "1",
+                display: "block",
+              },
+            }}
+            key={link.name}
+          >
+            <a
+              sx={{
+                color: "white",
+                textDecoration: "none",
+              }}
+              href={link.link}
+              aria-haspopup={link.subMenu && link.subMenu.length > 0 ? true : false}
+            >
+              {link.name}
+            </a>
+            {link.subMenu && link.subMenu.length > 0 ? (
+              <ul
+                sx={{
+                  listStyle: "none",
+                  m: 0,
+                  p: 0,
+                  backgroundColor: "darkorange",
+                  visibility: "hidden",
+                  opacity: "0",
+                  display: "none",
+                  minWidth: "8rem",
+                  position: "absolute",
+                  transition: "all 0.5s ease",
+                  marginTop: "1rem",
+                  left: "0",
+                  ":hover": {
+                    visibility: "visible",
+                    opacity: "1",
+                    display: "block",
+                  },
+                }}
+                aria-label="submenu"
+              >
+                {link.subMenu.map((subLink) => (
+                  <li
+                    sx={{
+                      clear: "both",
+                      width: "100%",
+                      padding: "1rem",
+                      ":hover": {
+                        backgroundColor: "red",
+                      },
+                    }}
+                    key={subLink.name}
+                  >
+                    <a
+                      sx={{
+                        color: "white",
+                        textDecoration: "none",
+                      }}
+                      href={subLink.link}
+                    >
+                      {subLink.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
           </li>
-          <li>
-            <Link to="/projects/">Projects</Link>
-          </li>
-          <li>
-            <Link to="/blog/">Blog</Link>
-          </li>
-          <li>
-            <Link to="/contact/">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+        ))}
+      </ul>
     </div>
-  </div>
-)
-
-export default Header
+  )
+}
