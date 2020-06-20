85f20b593b72db195c68ed37154113fbda59945c
Alex Rankine <alexrankine@college.harvard.edu>
2020-06-20 19:07:38 -0400

    add basic catalog

--- web/src/styles/theme.js
+++ web/src/styles/theme.js
@@ -1,20 +1,20 @@
 export default {
-  breakpoints: ['720px', '960px', '1300px', '1400px'],
+  breakpoints: ["720px", "960px", "1300px", "1400px"],
   colors: {
-    text: '#252121',
-    heading: '#252121',
-    monospace: '#252121',
-    background: 'white',
-    primary: '#C63F47',
-    secondary: '#A81A1A',
-    muted: '#BBABAB',
-    light: '#F9F9F9'
+    text: "#252121",
+    heading: "#252121",
+    monospace: "#252121",
+    background: "white",
+    primary: "#C63F47",
+    secondary: "#A81A1A",
+    muted: "#BBABAB",
+    light: "#F9F9F9"
   },
   fonts: {
-    body: 'Atlas Grotesk Web Regular, Akkurat, system-ui, sans-serif',
-    bold: 'Atlas Grotesk Web Bold, Akkurat-Bold, system-ui, sans-serif',
-    heading: 'Atlas Grotesk Web Bold, Akkurat-Bold, system-ui, sans-serif',
-    monospace: 'Akkurat-Mono, Menlo, monospace'
+    body: "Atlas Grotesk Web Regular, Akkurat, system-ui, sans-serif",
+    bold: "Atlas Grotesk Web Bold, Akkurat-Bold, system-ui, sans-serif",
+    heading: "Atlas Grotesk Web Bold, Akkurat-Bold, system-ui, sans-serif",
+    monospace: "Akkurat-Mono, Menlo, monospace"
   },
   lineHeights: {
     body: 1.5,
@@ -34,30 +34,30 @@ export default {
   },
   text: {
     default: {
-      color: 'text',
+      color: "text",
       fontSize: 3
     },
     caps: {
-      textTransform: 'uppercase',
-      letterSpacing: '0.05em'
+      textTransform: "uppercase",
+      letterSpacing: "0.05em"
     },
     heading: {
-      fontFamily: 'heading',
-      fontWeight: 'heading',
-      lineHeight: 'heading',
-      color: 'text',
-      textDecoration: 'none'
+      fontFamily: "heading",
+      fontWeight: "heading",
+      lineHeight: "heading",
+      color: "text",
+      textDecoration: "none"
     },
     caption: {
-      fontFamily: 'body',
-      fontWeight: 'body',
-      lineHeight: 'body',
+      fontFamily: "body",
+      fontWeight: "body",
+      lineHeight: "body",
       marginTop: [1.5, 2.5],
       fontSize: [1, 2]
     },
     small: {
-      fontFamily: 'body',
-      fontWeight: 'body',
+      fontFamily: "body",
+      fontWeight: "body",
       lineHeight: [1],
       fontSize: [1]
     }
@@ -66,45 +66,69 @@ export default {
     avatar: {
       width: 48,
       height: 48,
-      borderRadius: '50%'
+      borderRadius: "50%"
+    }
+  },
+  cards: {
+    list: {
+      borderRadius: 2,
+      backgroundColor: "light",
+      padding: 2,
+      "&:hover": {
+        bg: "muted"
+      }
+    }
+  },
+  buttons: {
+    primary: {
+      color: "background",
+      backgroundColor: "primary"
+    },
+    tag: {
+      padding: 2,
+      margin: 2,
+      variant: "text.small",
+      color: "background",
+      backgroundColor: "monospace",
+      font: "body"
     }
   },
   styles: {
     root: {
-      fontFamily: 'body',
-      fontWeight: 'body',
-      lineHeight: 'body',
-      backgroundColor: 'background'
+      fontFamily: "body",
+      fontWeight: "body",
+      lineHeight: "body",
+      backgroundColor: "background"
     },
     p: {
       fontSize: [2, 3, 3, 3]
     },
     h1: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: [5, 6, 7]
     },
     h2: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: [4, 5, 6]
     },
     h3: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: [3, 4],
-      marginTop: '0.5em'
+      marginTop: "0.5em"
     },
     h4: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: [2, 3],
-      marginTop: '0.5em'
+      marginTop: "0.5em"
     },
     a: {
-      fontWeight: 'bold',
-      color: 'inherit',
-      textDecoration: 'none'
+      fontWeight: "bold",
+      color: "inherit",
+      textDecoration: "none"
     },
     hr: {
-      color: 'text',
-      strokeWidth: '2px'
+      color: "text",
+      strokeWidth: "2px"
     }
   }
-}
+};
