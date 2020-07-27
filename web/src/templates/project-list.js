e171c22f82b3a2550cd49d19c66cbe0aee4755f9
Melissa Kwan <mwjkwan@gmail.com>
2020-07-26 17:48:00 -0700

    Create navigation between project pages

--- web/src/templates/project-list.js
+++ web/src/templates/project-list.js
@@ -1,11 +1,13 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { jsx, Button } from "theme-ui";
 import { graphql } from "gatsby";
 import Container from "../components/core/container";
 import GraphQLErrorList from "../components/core/graphql-error-list";
 import PreviewGrid from "../components/article-layouts/preview-grid";
 import SEO from "../components/core/seo";
 import Layout from "../containers/layout";
+import Link from "../components/core/link";
+import Spacer from "../components/core/spacer"
 import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from "../lib/helpers";
 
 export const query = graphql`
@@ -49,6 +51,12 @@ const ProjectListTemplate = (props) => {
     data &&
     data.projects &&
     mapEdgesToNodes(data.projects).filter(filterOutDocsWithoutSlugs);
+  
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "" : (currentPage - 1).toString()
+  const nextPage = (currentPage + 1).toString()
   return (
     <Layout>
       <SEO title="Projects" />
@@ -62,6 +70,23 @@ const ProjectListTemplate = (props) => {
             columns={[1, 3, 3]}
           />
         )}
+        <div style={{ textAlign: "center" }}>
+          {!isFirst && (
+            <Link to={`/projects/${prevPage}`} rel="prev">
+              <b>{`← Previous `}</b>
+            </Link>
+          )}
+          {Array.from({ length: numPages }, (_, i) => (
+            <Link key={`pagination-number${i + 1}`} to={`/projects/${i === 0 ? "" : i + 1}`}>
+              <b>{`   ${i + 1}   `}</b> 
+            </Link>
+          ))}
+          {!isLast && (
+            <Link to={`/projects/${nextPage}`} rel="next">
+              <b>{` Next →`}</b>
+            </Link>
+          )}
+        </div>
       </Container>
     </Layout>
   );
