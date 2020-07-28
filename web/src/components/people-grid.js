0ff942f1c049cec43dc27dd43e11e5766d5cedcb
Sahana Srinivasan <sahanas009@gmail.com>
2020-07-28 15:52:27 -0700

    attempted styling changes

--- web/src/components/people-grid.js
+++ web/src/components/people-grid.js
@@ -11,8 +11,8 @@ function ProfileCard({ image, name, _rawBio, position }) {
         {image && image.asset && (
           <Image
             src={imageUrlFor(buildImageObj(image))
-              .width(600)
-              .height(600)
+              .width(400)
+              .height(400)
               .fit("crop")
               .url()}
           />
@@ -21,9 +21,7 @@ function ProfileCard({ image, name, _rawBio, position }) {
       <Styled.h4>{name}</Styled.h4>
       <Styled.p>{position.title}</Styled.p>
       {_rawBio && (
-        <Text variant="small">
-          <BlockText blocks={_rawBio} />
-        </Text>
+          <BlockText blocks={_rawBio} style="profile"/>
       )}
     </div>
   );
@@ -33,7 +31,7 @@ function PeopleGrid({ items, title }) {
   return (
     <div>
       <Styled.h2>{title}</Styled.h2>
-      <Grid gap={3} columns={[1, 2, 3, 3]}>
+      <Grid gap={3} columns={[1, 2, 5]}>
         {items.map((item, key) => (
           <ProfileCard key={key} {...item} />
         ))}
