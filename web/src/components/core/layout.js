665dde6753a8c39dd0535328d48d2de747e0a221
Kev-Y-Huang <kev.y.huang@gmail.com>
2020-07-30 22:24:02 -0500

    Added Mailchimp subscription

--- web/src/components/core/layout.js
+++ web/src/components/core/layout.js
@@ -1,8 +1,7 @@
 /** @jsx jsx */
-import React from "react";
-import { jsx, Button, Input, Grid, Styled, Text } from "theme-ui";
+import React, { useState } from "react";
+import { jsx, Button, Input, Grid } from "theme-ui";
 import { SocialIcon } from "react-social-icons";
-import MailchimpSubscribe from "react-mailchimp-subscribe"
 import Header from "./header";
 import Container from "./container";
 import Link from "./link";
@@ -13,38 +12,122 @@ const socialIconStyles = {
   marginRight: "0.7em",
 };
 
-const url = "https://hodp.us20.list-manage.com/subscribe/post?u=4415ac3e9c1cf1f92c9e5b357&amp;id=8dbd4ffd38";
+const SubscribeWidget = ({ interestForm }) => {
+  const [email, setEmail] = useState("");
+  return (
+    <div
+      sx={{
+        p: 3,
+        borderRadius: "3px",
+        bg: "#FAFAFA",
+        border: "1px solid #111111",
+        maxWidth: "500px",
+      }}
+    >
+      <b>Subscribe to our monthly newsletter</b>
+      <Grid
+        as="form"
+        pt={3}
+        height="36px"
+        columns={["2.5fr 1fr"]}
+        action="https://hodp.us20.list-manage.com/subscribe/post"
+        method="POST"
+        noValidate
+      >
+        <input type="hidden" name="u" value="4415ac3e9c1cf1f92c9e5b357" />
+        <input type="hidden" name="id" value="8dbd4ffd38" />
+        <Input
+          type="email"
+          name="EMAIL"
+          id="MERGE0"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          mb={3}
+          sx={{ height: 4 }}
+        />
+        <div
+          style={{ position: "absolute", left: "-5000px" }}
+          aria-hidden="true"
+        >
+          <input
+            type="text"
+            name="b_eb05e4f830c2a04be30171b01_8281a64779"
+            tabIndex="-1"
+            value=""
+          />
+        </div>
+        <Button
+          type="submit"
+          name="subscribe"
+          id="mc-embedded-subscribe"
+          className="button"
+          sx={{
+            height: "32px",
+            display: "flex",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          Subscribe
+        </Button>
 
-const SubscribeWidget = ({ url, interestForm }) => (
-  <div
-    sx={{
-      p: 3,
-      borderRadius: "3px",
-      bg: "#FAFAFA",
-      border: "1px solid #111111",
-      maxWidth: "500px",
-    }}
-  >
-    The Harvard College Open Data Project (HODP) is a student-faculty group that aims to increase transparency
-    and solve problems on campus using public Harvard data.
-    {` Interested in open data? `}
-    <Link variant="highlighted" href={interestForm}>
-      <b>Join the team.</b>
-    </Link>
-    <br />
-    <br />
-    <iframe
-      src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FHarvardODP%2F&width=100&layout=button_count&action=like&size=large&height=30&appId"
-      width="100"
-      height="30"
-      style={{ border: "none", overflow: "hidden" }}
-      scrolling="no"
-      frameBorder="0"
-      allowTransparency="true"
-      allow="encrypted-media"
-    ></iframe>
-  </div>
-);
+        <div
+          style={{ position: "absolute", left: "-5000px" }}
+          aria-hidden="true"
+          aria-label="Please leave the following three fields empty"
+        >
+          <label htmlFor="b_name">Name: </label>
+          <input
+            type="text"
+            name="b_name"
+            tabIndex="-1"
+            value=""
+            placeholder="Freddie"
+            id="b_name"
+          />
+
+          <label htmlFor="b_email">Email: </label>
+          <input
+            type="email"
+            name="b_email"
+            tabIndex="-1"
+            value=""
+            placeholder="youremail@gmail.com"
+            id="b_email"
+          />
+
+          <label htmlFor="b_comment">Comment: </label>
+          <textarea
+            name="b_comment"
+            tabIndex="-1"
+            placeholder="Please comment"
+            id="b_comment"
+          />
+        </div>
+      </Grid>
+      <br />
+      The Harvard College Open Data Project (HODP) is a student-faculty group
+      that aims to increase transparency and solve problems on campus using
+      public Harvard data.
+      {` Interested in open data? `}
+      <Link variant="highlighted" href={interestForm}>
+        <b>Join the team.</b>
+      </Link>
+      <br />
+      <br />
+      <iframe
+        src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FHarvardODP%2F&width=100&layout=button_count&action=like&size=large&height=30&appId"
+        width="100"
+        height="30"
+        style={{ border: "none", overflow: "hidden" }}
+        scrolling="no"
+        frameBorder="0"
+        allowTransparency="true"
+        allow="encrypted-media"
+      />
+    </div>
+  );
+};
 
 const Layout = ({
   children,
