e3073743b222b58de865375b8f5142f4cbca5ff1
Melissa Kwan <mwjkwan@gmail.com>
2020-08-05 09:48:39 -0700

    Make grid responsive

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -85,7 +85,7 @@ const AboutPage = (props) => {
     <Layout>
       <SEO title={page.title} />
       <Container>
-        <Grid gap={5} columns={[1, 1, 2]}>
+        <Grid gap={[2, 5, 5]} columns={[1, 1, 2]}>
           <BlockContent blocks={(page._rawBody && page._rawBody[0]) || []} />
           <div>
             <BlockContent blocks={(page._rawBody && page._rawBody.slice(1)) || []} />
@@ -104,7 +104,7 @@ const AboutPage = (props) => {
         <Container>
           <Spacer height={5} />
           <Styled.h1>Our work</Styled.h1>
-          <Grid gap={[1, 4, 4]} columns={[1, 2, 4]}>
+          <Grid gap={4} columns={[1, 2, 4]}>
             {page._rawBodySecondary && page._rawBodySecondary.map((block) =>
               <FeatureCard {...block} />
             )}
