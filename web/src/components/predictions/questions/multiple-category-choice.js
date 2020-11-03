e657b220f1997be851382182e6811533b694f8e2
Kev-Y-Huang <kev.y.huang@gmail.com>
2020-11-02 19:03:20 -0600

    Multi category choice now is multi thumb range

--- web/src/components/predictions/questions/multiple-category-choice.js
+++ web/src/components/predictions/questions/multiple-category-choice.js
@@ -1,73 +1,89 @@
 import React, { useState } from "react";
-import { Box, Label } from "theme-ui";
+import { Box, Text } from "theme-ui";
 import { Range } from "react-range";
 import Thumb from "./thumb";
 import Track from "./track";
 
+// RGB Values for #C63F3F and #ccc
+const start_red = 198;
+const start_green = 63;
+const start_blue = 63;
+const end_red = 204;
+const end_green = 204;
+const end_blue = 204;
+
+// Rounding to first decimal value
+function decimalRound(val) {
+  return Math.round(val * 10) / 10;
+}
+
 function MultipleCategoryChoice(props) {
-  const [values, setValues] = useState(
-    Array(props.choices.length).fill({ max: 100, current: 0 })
-  );
+  const length = props.choices.length;
+  const arr = [];
+  const displayArr = [];
+
+  // Populate with gradient color values between #C63F3F and #ccc
+  let colors = Array.from(Array(length).keys()).map((index) => {
+    const pct = index / (length - 1);
+    const diffRed = (end_red - start_red) * pct + start_red;
+    const diffGreen = (end_green - start_green) * pct + start_green;
+    const diffBlue = (end_blue - start_blue) * pct + start_blue;
+    return `rgb(${diffRed},${diffGreen},${diffBlue})`;
+  });
+
+  // Populate arr with values of thumbs and displayArr with displayed ranges
+  for (let i = 0; i < length - 1; i++) {
+    const val = 100 / length;
+    arr.push((i + 1) * val);
+    displayArr.push(decimalRound(val));
+  }
+  displayArr.push(decimalRound(100 - arr[length - 2]));
+
+  const [values, setValues] = useState(arr);
+  const [displayValues, setDisplayValues] = useState(displayArr);
 
   const afterSubmission = (event) => {
     event.preventDefault();
   };
 
-  const handleChange = (value, index) => {
-    const newValues = values.map((slider, i) => {
-      if (index === i) {
-        return {
-          max: slider.max,
-          current: value,
-        };
-      } else {
-        return {
-          max: slider.max + values[index].current - value,
-          current: slider.current,
-        };
-      }
-    });
-    setValues(newValues);
+  // Populating with new values on change
+  const handleChange = (values) => {
+    setValues(values);
+    const newValues = values.map((val, i) =>
+      i === 0 ? decimalRound(val) : decimalRound(val - values[i - 1])
+    );
+    newValues.push(decimalRound(100 - values[length - 2]));
+    setDisplayValues(newValues);
   };
 
   return (
     <form onSubmit={(event) => afterSubmission(event)}>
-      {props.choices &&
-        props.choices.map((choice, index) => (
-          <Box>
-            <Label>
-              {choice.name}: {values[index].current.toFixed(1)}
-            </Label>
-            <Box mt={1} mx={3}>
-              <Range
-                name={choice.name}
-                id={index}
-                step={props.step}
-                max={values[index].max}
-                min={0}
-                values={[values[index].current]}
-                onChange={(values) => handleChange(values[0], index)}
-                renderTrack={({ props, children }) => (
-                  <Track
-                    {...props}
-                    values={[values[index].current]}
-                    upper={values[index].max}
-                    lower={0}
-                    colors={["#C63F3F", "#ccc"]}
-                  >
-                    {children}
-                  </Track>
-                )}
-                renderThumb={({ props }) => (
-                  <Thumb
-                    val={values[index].current.toFixed(1)}
-                    thumbProps={props}
-                  />
-                )}
-              />
-            </Box>
-          </Box>
-        ))}
+      <Box mt={1} mx={3}>
+        <Range
+          draggableTrack
+          values={values}
+          step={props.step}
+          min={0}
+          max={100}
+          onChange={(values) => handleChange(values)}
+          renderTrack={({ props, children }) => (
+            <Track
+              {...props}
+              values={values}
+              upper={100}
+              lower={0}
+              colors={colors}
+            >
+              {children}
+            </Track>
+          )}
+          renderThumb={({ props }) => <Thumb val={""} thumbProps={props} />}
+        />
+        {displayValues &&
+          displayValues.map((val, i) => (
+            <Text>{`Option${i + 1}: ${val}`}</Text>
+          ))}
+      </Box>
     </form>
   );
 }
--- web/src/components/predictions/questions/thumb.js
+++ web/src/components/predictions/questions/thumb.js
@@ -1,36 +1,43 @@
 import React from "react";
 
-const Thumb = (props) => (
-  <div
-    {...props.thumbProps}
-    style={{
-      ...props.thumbProps.style,
-      height: "0.8rem",
-      width: "0.8rem",
-      borderRadius: "0.3rem",
-      backgroundColor: "#FFF",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      boxShadow: "0px 0.15rem 0.45rem #AAA",
-    }}
-  >
+const Thumb = (props) => {
+  const renderValue =
+    props.val === "" ? null : (
+      <div
+        style={{
+          position: "absolute",
+          top: "-2rem",
+          color: "#fff",
+          fontWeight: "bold",
+          fontSize: "1rem",
+          fontFamily: "Arial,Helvetica Neue,Helvetica,sans-serif",
+          padding: "0.3rem",
+          borderRadius: "0.3rem",
+          backgroundColor: "#2F2F2F",
+        }}
+      >
+        {props.val}
+      </div>
+    );
+
+  return (
     <div
+      {...props.thumbProps}
       style={{
-        position: "absolute",
-        top: "-2rem",
-        color: "#fff",
-        fontWeight: "bold",
-        fontSize: "1rem",
-        fontFamily: "Arial,Helvetica Neue,Helvetica,sans-serif",
-        padding: "0.3rem",
+        ...props.thumbProps.style,
+        height: "0.8rem",
+        width: "0.8rem",
         borderRadius: "0.3rem",
-        backgroundColor: "#2F2F2F",
+        backgroundColor: "#FFF",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        boxShadow: "0rem 0.15rem 0.45rem #AAA",
       }}
     >
-      {props.val}
+      {renderValue}
     </div>
-  </div>
-);
+  );
+};
 
 export default Thumb;
