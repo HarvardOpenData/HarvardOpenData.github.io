2a4e519732c37842653c9cf085c1b216249a79ff
Melissa Kwan <mwjkwan@gmail.com>
2020-11-02 21:31:30 -0500

    Remove references to gated content

--- web/src/pages/surveys.js
+++ web/src/pages/surveys.js
@@ -3,13 +3,11 @@ import { jsx, Grid, Styled } from "theme-ui";
 import { graphql } from "gatsby";
 import BlockContent from "../components/block-content";
 import Container from "../components/core/container";
-import BannerHeader from "../components/core/banner-header";
 import GraphQLErrorList from "../components/core/graphql-error-list";
 import SEO from "../components/core/seo";
 import Layout from "../containers/layout";
 import Section from "../components/core/section";
 import Login from "../components/users/login";
-import GatedContent from "../components/users/gated-content";
 import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from "../lib/helpers";
 
 export const query = graphql`
@@ -49,11 +47,6 @@ const SurveysPage = (props) => {
         <Grid gap={[4, 5, 6]} columns={[1, 1, "3fr 1fr"]}>
           <div>
             <Styled.h1>Survey Group</Styled.h1>
-            <Login />
-            <GatedContent
-              renderPublic={() => <div>Hello generic person</div>}
-              renderPrivate={(user) => <div>Hello {user.displayName}</div>}
-            />
             <BlockContent blocks={page._rawBody || []} />
           </div>
           <Section header="Survey Projects" className="small preview">
