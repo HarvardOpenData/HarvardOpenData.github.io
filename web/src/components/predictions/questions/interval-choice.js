a0d185abd4b1ba261a91db1c400c94ccad39eee5
matthewqu14 <matthewqu@college.harvard.edu>
2021-01-31 21:56:46 -0500

    added input field for range questions

--- web/src/components/predictions/questions/interval-choice.js
+++ web/src/components/predictions/questions/interval-choice.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import useStateWithCallback from 'use-state-with-callback';
 import firebase from "gatsby-plugin-firebase";
-import { Box, Grid, Text } from "theme-ui";
+import { Box, Grid, Text, Input } from "theme-ui";
 import { Range } from "react-range";
 import { format } from "date-fns";
 import Thumb from "./thumb";
@@ -13,6 +14,13 @@ function IntervalChoice(props) {
     props.prediction ? props.prediction : [lower, upper]
   );
 
+  useEffect(() => {
+      updateFirebase();
+  }, [values]);
+
+  // const [values, setValues] = useStateWithCallback(
+  //     props.prediction ? props.prediction : [lower, upper]
+  // );
   const afterSubmission = (event) => {
     event.preventDefault();
   };
@@ -29,6 +37,59 @@ function IntervalChoice(props) {
     }
   };
 
+  const validateLower = (e) => {
+      const bound = Math.min(values[1], upper);
+      let value = parseInt(e.target.value);
+      if (value < lower || e.target.value === "") {
+          value = lower;
+      } else if (value > bound) {
+          value = bound;
+      }
+      setValues([value, values[1]]);
+  };
+
+  const validateUpper = (e) => {
+      const bound = Math.max(values[0], lower);
+      let value = parseInt(e.target.value);
+      if (value > upper || e.target.value === "") {
+          value = upper;
+      } else if (value < bound) {
+          value = bound;
+      }
+      setValues([values[0], value]);
+  };
+
+  const validateValues = (values) => {
+    let min = values[0];
+    let max = values[1];
+
+      if (min < lower) {
+          min = lower;
+      } else if (min > upper) {
+          min = upper;
+      }
+
+      if (max < lower) {
+          max = lower;
+      } else if (max > upper) {
+          max = upper;
+      }
+
+      return [min, max]
+  };
+
+  function validateThumb(index) {
+      if (values[index] === "") {
+          return lower
+      } else if (values[index] < lower) {
+          return lower
+      } else if (values[index] > upper) {
+          return upper
+      } else {
+          return values[index]
+      }
+  }
+
   return (
     <form onSubmit={(event) => afterSubmission(event)}>
       <Grid mt={1} mx={3} gap={2} columns={[1, "3fr 5fr"]}>
@@ -42,8 +103,37 @@ function IntervalChoice(props) {
             {props.name}
           </Text>
           <Text sx={{ fontSize: 2 }}>
-            Your prediction: {values.join(" - ")}
+            Your prediction:
           </Text>
+          <Input
+              sx={{
+                  width: "35%",
+                  display: "inline",
+                  p: 1
+              }}
+              type='number'
+              min={lower}
+              max={upper}
+              value={values[0]}
+              onChange={(e) => setValues([e.target.value && parseInt(e.target.value), values[1]])}
+              onBlur={validateLower}
+              onKeyDown={(e) => { if (e.key === "Enter") e.target.blur(); }}
+          />
+          {" - "}
+          <Input
+              sx={{
+                  width: "35%",
+                  display: "inline",
+                  p: 1
+              }}
+              type='number'
+              min={lower}
+              max={upper}
+              value={values[1]}
+              onChange={(e) => setValues([values[0], e.target.value && parseInt(e.target.value)])}
+              onBlur={validateUpper}
+              onKeyDown={(e) => { if (e.key === "Enter") e.target.blur(); }}
+          />
           <Text sx={{ fontSize: 1, color: "gray" }}>
             {props.disabled ? "Expired" : "Expires"} on{" "}
             {format(date_expired, "MM-DD-YYYY")}
@@ -52,7 +142,7 @@ function IntervalChoice(props) {
         <Range
           disabled={props.disabled}
           draggableTrack
-          values={values}
+          values={validateValues(values)}
           step={props.step}
           min={lower}
           max={upper}
@@ -70,7 +160,7 @@ function IntervalChoice(props) {
             </Track>
           )}
           renderThumb={({ index, props }) => (
-            <Thumb val={values[index]} thumbProps={props} />
+            <Thumb val={validateThumb(index)} thumbProps={props} />
           )}
         />
         {props.explanation}
