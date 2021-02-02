e0565e96e956bfbb58aa066e593b31f524f63ecd
Kev-Y-Huang <kev.y.huang@gmail.com>
2021-02-02 09:53:26 -0500

    Add custom eslint file

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -67,6 +67,7 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-theme-ui",
+    "gatsby-plugin-eslint",
     {
       resolve: "gatsby-source-sanity",
       options: {
