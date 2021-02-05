121ffa3d6b0f9234beb140b3d3e252cafbd8f0d9
Kev-Y-Huang <kev.y.huang@gmail.com>
2021-02-05 18:48:32 -0500

    prettier applied and styling the IFrame component

--- web/src/components/block-content/iframe.js
+++ web/src/components/block-content/iframe.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { Text } from "theme-ui";
+import { jsx, Text } from "theme-ui";
 
 function IFrame(props) {
-    return (
-        <div style={{alignItems: "center"}}>
-            <br />
-            {props.url && <iframe src={props.url}/>}
-            <Text variant="caption">{props.caption}</Text>
-            <br />
-        </div>
-    );
+  return (
+    <div style={{ alignItems: "center" }}>
+      <br />
+      {props.url && (
+        <iframe
+          src={props.url}
+          scrolling={"no"}
+          style={{ border: "None" }}
+          seamless={"seamless"}
+          height={"525"}
+          width={"100%"}
+        />
+      )}
+      <Text variant="caption">{props.caption}</Text>
+      <br />
+    </div>
+  );
 }
 
 export default IFrame;
--- web/src/components/block-content/plotly-interactive.js
+++ web/src/components/block-content/plotly-interactive.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { Alert, Spinner, Text } from "theme-ui";
+import { jsx, Alert, Spinner, Text } from "theme-ui";
 import Loadable from "react-loadable";
 
 const Plotly = Loadable({
   loader: () => import(`react-plotly.js`),
   loading: ({ timedOut }) =>
-    timedOut ? (
-      <Alert>Error: Loading Plotly timed out.</Alert>
-    ) : (
-      <Spinner />
-    ),
+    timedOut ? <Alert>Error: Loading Plotly timed out.</Alert> : <Spinner />,
   timeout: 10000,
 });
 
 function PlotlyInteractive(props) {
   return (
-    <div style={{alignItems: "center"}}>
+    <div style={{ alignItems: "center" }}>
       <br />
       {props.json && <Plotly {...JSON.parse(props.json)} />}
       <Text variant="caption">{props.caption}</Text>
