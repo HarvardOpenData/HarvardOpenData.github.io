15fde814ffec33dab3b7f7d1b742e832fa3b108e
Kevin Huang <kev.y.huang@gmail.com>
2022-10-11 16:50:56 -0400

    Better return handling and readme

--- web/functions/slack-bot/slack-bot.js
+++ web/functions/slack-bot/slack-bot.js
@@ -4,9 +4,7 @@ const fetch = require("node-fetch");
 const sanityClient = require("@sanity/client");
 
 const client = sanityClient({
-  // projectId: process.env.SANITY_PROJECTID,
   projectId: "xx0obpjv",
-  // dataset: process.env.SANITY_DATASET,
   dataset: "production",
   token: process.env.SANITY_WRITE_TOKEN,
   useCdn: false,
@@ -15,35 +13,30 @@ const client = sanityClient({
 // Function for parsing slack parameters from http body
 // Source: https://www.sitepoint.com/get-url-parameters-with-javascript/
 function parseParameters(queryString) {
-  // we'll store the parameters here
-  var obj = {};
+  var params = {};
 
   // split our query string into its component parts
-  var arr = queryString.split("&");
+  var arr = decodeURIComponent(queryString).split("&");
 
   for (var i = 0; i < arr.length; i++) {
     // separate the keys and the values
     var a = arr[i].split("=");
 
-    // set parameter name and value (use 'true' if empty)
-    var paramName = a[0];
-    var paramValue = typeof a[1] === "undefined" ? true : a[1];
-
-    obj[paramName] = paramValue;
+    params[a[0]] = a[1];
   }
 
-  return obj;
+  return params;
 }
 
 exports.handler = async function (event) {
-  console.log(event);
   const { channel_id, command, text } = parseParameters(event.body);
   const params = text.split("+");
   const slug = params[1];
   const url = params[2];
+  let blocks = "";
 
   if (channel_id == "C03JMGCRM9B") {
-    if (command == "%2Fredirect") {
+    if (command == "/redirect") {
       const document = {
         _type: "redirect",
         // Some workflow state
@@ -54,11 +47,17 @@ exports.handler = async function (event) {
         },
         url,
       };
-      let blocks = "";
 
-      try {
-        await client.create(document);
-        blocks = [
+      blocks = await client
+        .create(document)
+        .then(() => {
+          const sanityUrl = `https://api.netlify.com/build_hooks/${process.env.SANITY_STUDIO_BUILD_HOOK_ID}`;
+          return fetch(sanityUrl, {
+            method: "POST",
+            body: {},
+          });
+        })
+        .then(() => [
           {
             type: "section",
             text: {
@@ -66,44 +65,53 @@ exports.handler = async function (event) {
               text: `Redirect created at https://hodp.org/${slug} to ${url}.`,
             },
           },
-        ];
-      } catch (error) {
-        console.log(error);
-        blocks = [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `Redirect was unable to be created at https://hodp.org/${slug} with ${url}.`,
+        ])
+        .catch((error) => {
+          console.log(error);
+          return [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `Redirect was unable to be created at https://hodp.org/${slug} with ${url}.`,
+              },
             },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "Please check again that you typed in the right parameters.",
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "Please check again that you typed in the right parameters.",
+              },
             },
-          },
-        ];
-      } finally {
-        return {
-          statusCode: 200,
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ blocks }),
-        };
-      }
+          ];
+        });
     } else {
-      return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: `${command.slice(3)} is not a supported command at this moment.`,
-      };
+      blocks = [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `${command.slice(
+              3
+            )} is not a supported command at this moment.`,
+          },
+        },
+      ];
     }
   } else {
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: "You are not authorized to use this Jimmie Jams.",
-    };
+    blocks = [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "You are not authorized to use this Jimmie Jams.",
+        },
+      },
+    ];
   }
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ blocks }),
+  };
 };
